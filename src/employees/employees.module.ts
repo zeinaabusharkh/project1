b7d1f164839dbef2b6bc056ddd05a092961c1657
Xena <raedzeina@gmail.com>
2025-04-09 08:41:26 -0500

    fix

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [EmployeesController],
   providers: [EmployeesService],
 })
